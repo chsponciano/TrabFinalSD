050fe740f72f3b7601ba0da3c5b97f6e54de51d6
Carlos <chs.ponciano@gmail.com>
2019-06-22 11:39:14 -0300

    alterações

--- ClientApp/index.py
+++ ClientApp/index.py
@@ -1,33 +1,59 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 import subprocess
+import pickle
+from node import Node
 
 app = Flask(__name__)
 contador = 1
-
+dict_nodes = dict()
+file_seralization = 'graph.dat'
 
 @app.route('/')
 def index():
+    get_dict()
     return render_template('index.html')
 
-# @app.route('/add', methods=['POST',])
-@app.route('/add')
+@app.route('/add', methods=['POST',])
 def add_server():
     global contador
-    #id_server = request.form['id_server']
-    #cust_server = request.form['cust_server']
-
-    with open("output.log", "a") as output:
-        subprocess.call("docker run -d --network graph-network chsponciano/noderabbit python ServerAppInitialize.py q" + str(contador),
-                        shell=True, stdout=output, stderr=output)
-
+    global dict_nodes
+    idx = str(request.form['id_server'] + "_" + str(contador))
+    dict_nodes.update({idx, Node(idx, str(request.form['cust_server']))})
     contador += 1
-    return redirect(url_for('index'))
+    save_dict()
 
+@app.route('/stop', methods=['POST',])
+def stop_server():
+    global dict_nodes
+    dict_nodes.get(str(request.form['id_server'])).stop()
+    save_dict()
 
-@app.route('/route')
-def calcule_router():
-    return redirect(url_for('index'))
+@app.route('/start', methods=['POST',])
+def start_server():
+    global dict_nodes
+    dict_nodes.get(str(request.form['id_server'])).start()
+    save_dict()
 
+@app.route('/route', methods=['POST',])
+def calc_router():
+    node1 = request.form['node_one_server']
+    node2 = request.form['node_two_server']
+    algorithm = request.form['algorithm_server']
+    print(f'{node1} - {node2} - {algorithm}')
+
+def save_dict():
+    global file_seralization
+    global dict_nodes
+    fl = open(file_seralization, 'wb')
+    pickle.dump(dict_nodes, fl)
+    fl.close()
+
+def get_dict():
+    global file_seralization
+    global dict_nodes
+    fl = open(file_seralization, 'rb')
+    dict_nodes.update({pickle.load(fl)})
+    fl.close()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
